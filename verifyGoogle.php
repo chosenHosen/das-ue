04a26a7c810f600cb6f09bad81d45ce233c9b09b
Sebastian Stricker <sebastian@stricker.at>
2018-05-22 16:14:58 +0200

    Google Authentication remember working

--- verifyGoogle.php
+++ verifyGoogle.php
@@ -8,16 +8,47 @@ $client = new Google_Client(['client_id' => '280359176332-30opuubdvu7gb7pjhq2nul
 $payload = $client->verifyIdToken($_POST["idtoken"]);
 if ($payload)
 {
- // $userid = $payload['sub'];
-  //valid login
-	$_SESSION["auth"] = sha1(openssl_random_pseudo_bytes(20));
-	if(isset($_POST["remember"]) && $_POST["remember"] === "on")
+  	//valid login
+ 	$userid = $payload['sub'];
+
+
+    
+    $_SESSION["user"] = $userid;
+    $_SESSION["auth"] = sha1(openssl_random_pseudo_bytes(20));
+    $_SESSION["logged_in"] = 1;
+
+    if(isset($_POST["remember"]) && $_POST["remember"] === "on")
 	{
-	    setcookie("auth", $_SESSION["auth"], (int)(time() + 60*60*24*365), "/", "", true);
-	}
-	$_SESSION["logged_in"] = 1;
-	header('Location: index.php');
-	die();
+		//set cookies
+        $time = (int)(time() + 60*60*24*365);
+        setcookie("auth", $_SESSION["auth"], $time, "/", $_SERVER["SERVER_NAME"], true);
+        setcookie("user", $userid, $time, "/", $_SERVER["SERVER_NAME"], true);
+
+    	//check for entry
+		include "db_connect.php";
+		$query = $conn->prepare("SELECT * FROM users WHERE username=?");
+		$query->bind_param("s", $userid); 
+		$query->execute();
+		if($query->fetch()) //update entry
+		{
+
+	        include "db_connect.php";
+	        $query = $conn->prepare("UPDATE users SET cookie_auth=? WHERE username=?");
+	        $query->bind_param("ss",$_SESSION["auth"], $userid);
+	        $query->execute();
+		}
+		else //new entry
+		{
+			$password = randomPassword();
+			$password = password_hash($password, PASSWORD_DEFAULT);
+		    include "db_connect.php";
+            $query = $conn->prepare("INSERT INTO users (`username`, `password`, `http_digest_a1`, `cookie_auth`) VALUES (?,?,?,?)");
+            $query->bind_param("ssss", $userid, $password, $password, $_SESSION["auth"]);
+            $query->execute();
+		}
+    }
+    header('Location: index.php');
+    die();
 } 
 else
 {	
@@ -29,6 +60,17 @@ function abort()
     $_SESSION["logged_in"] = 1;
     header('Location: index.php');
     die();
+}
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
 }
 
 ?>
